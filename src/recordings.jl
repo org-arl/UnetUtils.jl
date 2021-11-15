d52cf30efdb08792563205631ba1deabea036b52
Mandar Chitre <mandar@nus.edu.sg>
2021-11-15 22:35:26 +0800

    refactor(rec): improve code design

--- src/recordings.jl
+++ src/recordings.jl
@@ -4,7 +4,7 @@ import SignalAnalysis: signal, framerate, nchannels
 import Statistics: mean
 import DataFrames: DataFrame, DataFrameRow
 import TimeZones: localzone, ZonedDateTime, astimezone, @tz_str
-import Dates: unix2datetime, datetime2unix, now
+import Dates: unix2datetime, datetime2unix, now, UTC
 
 const MAGIC = hton(0x43c04d126f173001)
 
@@ -18,21 +18,22 @@ function read(filename; tz=localzone())
   for filename ∈ readdir(filename; join=true)
     m = match(r"/?rec\-(\d+)\.dat", filename)
     if m !== nothing
-      t = astimezone(ZonedDateTime(unix2datetime(parse(Int64, m[1]) / 1000), tz"UTC"), tz)
+      t = 0
       filesize = stat(filename).size
       nch = NCHANNELS
       fs = FRAMERATE
       if filesize > 24
-        open(filename, "r") do io
-          x = Base.read(io, UInt64)
-          if x == MAGIC
-            Base.read(io, Int64)
-            fs = Int(Base.read(io, Int32))
-            nch = Int(Base.read(io, Int16))
+        open(filename, "r") do f
+          hdr = read(f, RecordingHeader)
+          if hdr !== nothing
+            t = astimezone(ZonedDateTime(unix2datetime(hdr.millis / 1000), tz"UTC"), tz)
+            nch = hdr.nchannels
+            fs = hdr.framerate
           end
         end
       end
       d = filesize / (4 * nch * fs)
+      t == 0 && (t = astimezone(ZonedDateTime(unix2datetime(parse(Int64, m[1]) / 1000), tz"UTC"), tz))
       push!(df, (t, filename, d, nch, Float64(fs)))
     end
   end
@@ -88,11 +89,15 @@ end
 
 function write(filename, x; fs=framerate(x))
   open(filename, "w") do io
-    Base.write(io, MAGIC)
-    Base.write(io, round(Int64, 1000 * datetime2unix(now())))
-    Base.write(io, round(Int32, fs))
-    Base.write(io, Int16(nchannels(x)))
-    Base.write(io, zeros(Int16, 5))
+    hdr = RecordingHeader(
+      millis = round(Int64, 1000 * datetime2unix(now(UTC))),
+      framerate = round(Int32, fs),
+      nchannels = nchannels(x))
+    Base.write(io, hdr.magic)
+    Base.write(io, hdr.millis)
+    Base.write(io, hdr.framerate)
+    Base.write(io, hdr.nchannels)
+    Base.write(io, UInt16[0, 0, 0, 0, 0])
     Base.write(io, Float32.(x'))
   end
   nothing
